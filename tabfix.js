570e91fdeff6b4cee6019824950921963c32ad3d
BOakes <oakes.ben@outlook.com>
2022-12-13 19:07:04 -0600

    updated main panels

---
+++ tabfix.js
@@ -0,0 +1,43 @@
+// Taken from https://github.com/botoxparty/XP.css/issues/38
+// This makes the tabs work :)
+
+document.addEventListener("DOMContentLoaded", function() {
+    const tabs = document.querySelectorAll("menu[role=tablist]");
+    
+    for (let i = 0; i < tabs.length; i++) {
+      const tab = tabs[i];
+    
+      const tabButtons = tab.querySelectorAll("menu[role=tablist] > button");
+    
+      tabButtons.forEach((btn) =>
+        btn.addEventListener("click", (e) => {
+          e.preventDefault();
+    
+          tabButtons.forEach((button) => {
+            if (
+              button.getAttribute("aria-controls") ===
+              e.target.getAttribute("aria-controls")
+            ) {
+              button.setAttribute("aria-selected", true);
+              openTab(e, tab);
+            } else {
+              button.setAttribute("aria-selected", false);
+            }
+          });
+        })
+      );
+    }
+      
+    });
+    
+    
+    function openTab(event, tab) {
+      const articles = tab.parentNode.querySelectorAll('[role="tabpanel"]');
+      articles.forEach((p) => {
+        p.setAttribute("hidden", true);
+      });
+      const article = tab.parentNode.querySelector(
+        `[role="tabpanel"]#${event.target.getAttribute("aria-controls")}`
+      );
+      article.removeAttribute("hidden");
+    }
